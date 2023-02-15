0964797e51bc242cae178c7e45aa985f7fc65985
unknown <mprakhar35@gmail.com>
2023-02-15 14:30:29 +0530

    made required changes

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useState, useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import BASE_URL from '../api_url';
 
