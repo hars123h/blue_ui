4a017836e5e99c7c7d34ed4ca5d13af815022f38
unknown <mprakhar35@gmail.com>
2023-03-05 18:41:57 +0530

    some changes

--- src/api_url.js
+++ src/api_url.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://wallet-5klca.ondigitalocean.app/api";
+const BASE_URL = "https://urchin-app-xhb2n.ondigitalocean.app/api";
 //  const BASE_URL = "http://localhost:5000/api";
 // 
 export default BASE_URL;
